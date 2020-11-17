7c71bd38576e83a7840ac80367d718f22e6d78ea
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-17 18:08:32 +0100

    rollback home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @car_preferences = Car.new
-    @rental_preferences = Rental.new
-    @rentals = Rental.all
+    # @rental_preferences = Rental.new
+    # @rentals = Rental.all
   end
 end
