e37a45987367e742644bd6e5699ce09d5a01447c
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-19 14:32:52 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_19_102814) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +44,9 @@ ActiveRecord::Schema.define(version: 2020_11_19_102814) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
     t.integer "capacity"
-    t.text "description"
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
