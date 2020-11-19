795abf8a89ced83856c2fddbb3a5d23de8e1c11b
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-19 12:22:11 +0100

    reviews display in the show of the car

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,21 @@ class ReviewsController < ApplicationController
     @car = Car.find(params[:car_id])
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    @car = Car.find(params[:car_id])
+    @review.car = @car
+    if @review.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
