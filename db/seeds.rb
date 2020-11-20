0095f8440d91c0a149f372f8e390a409e08c980e
Thomas Monvoisin <thomasmonvoisin@gmail.com>
2020-11-20 13:38:22 +0100

    Finished index and imrpoved seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,20 @@ Car.destroy_all
 puts "Destroying all rentals"
 Rental.destroy_all
 puts "Creating cars"
-40.times do 
+40.times do
   car = Car.new(
     model: Faker::Vehicle.make_and_model,
     price: rand(20..99),
     capacity: rand(1..8),
     description: Faker::Vehicle.car_options.join(","),
-    location: %w[Paris lyon Bordeaux Marseille Lille Nantes].sample,
+    location: "#{rand(1..99)} #{["rue de la paix", "12 Avenue de la république", "Rue de l'Église", "Rue de la Gare", "Rue Pasteur", "Rue Jean Jaures"].sample} #{%w[Paris lyon Bordeaux Marseille Lille Nantes Toulouse Grenoble].sample}",
     user_id: User.all.ids.sample.to_i
     )
   car.save!
   puts "Created #{car.model}"
 end
+
+# file = URI.open('https://tof.cx/images/2019/11/22/acff3a8d5f457ffe5e6a60abf2c8a6ac.png')
+# lesupplier1 = User.new(email: "barnabelemoulu@gmail.com", password: "password", first_name: "Barnabé", last_name: "Le Moulu", phone_number: "0143306970", birthdate: "1819-12-24", address: "24 Rue Vauvenargues", city: "Paris", zipcode: "75018", bio: "Je suis né des amours interdites d'une chèvre et d'un courtier en assurances. Je suis Légion. J'accepte les paiements en bitcoins.", insta: "https://www.instagram.com/instagram/", linkedin: "https://www.facebook.com/bernard.lemoullec")
+# lesupplier1.avatar.attach(io: file, filename: 'some-image.jpg', content_type: 'image/jpg')
+# lesupplier1.save!
